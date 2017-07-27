ff302d1aca608a1a6e796540f8b8f7b9a0590fbf
David Marland <davidm@rland.me.uk>
2017-07-27 22:54:36 +0100

    Try to build main application

--- public/index.php
+++ public/index.php
@@ -11,3 +11,5 @@ $date = new DateTimeImmutable();
 echo '<h1>PHP Example application</h1>';
 echo '<h2>Example data from Model with dependency: ' . $model->getValue() . '</h2>';
 echo '<h3>Date: ' . $date->format(DateTime::ISO8601);
+
+## todo - add an environment variable
